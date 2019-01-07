7d687212f440a42b598f4147a5ed911c94e541f0
Kristian Samuelsson <kristian.samuelsson@valtech.se>
2019-01-07 08:53:35 +0100

    initial commit

---
+++ DockerTest/Controllers/DebugController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace DockerTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DebugController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return Environment.GetEnvironmentVariable("COMPUTERNAME") ??
+                   Environment.GetEnvironmentVariable("HOSTNAME");
+        }
+
+    }
+}
