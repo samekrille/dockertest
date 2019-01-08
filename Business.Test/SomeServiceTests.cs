65329d4d66cdc5e2f5eafd22d958918fcb4a8687
Kristian Samuelsson <kristian.samuelsson@valtech.se>
2019-01-08 14:07:02 +0100

    Added running tests

---
+++ Business.Test/SomeServiceTests.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+
+namespace Business.Test
+{
+    [TestFixture]
+    public class SomeServiceTests
+    {
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.Pass();
+        }
+    }
+}
