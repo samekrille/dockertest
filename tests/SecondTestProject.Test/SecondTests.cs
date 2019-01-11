8264bce85d6bd87e3fdc94f3b2944d4f515624a3
Kristian Samuelsson <kristian.samuelsson@valtech.se>
2019-01-11 10:51:15 +0100

    Added second test project

---
+++ tests/SecondTestProject.Test/SecondTests.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+
+namespace SecondTestProject.Test
+{
+    [TestFixture]
+    public class SecondTests
+    {
+
+        [Test]
+        public void Test3()
+        {
+            Assert.Pass();
+        }
+    }
+}
